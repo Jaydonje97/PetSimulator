17ae8cf8120ab3d7ee45acb1d3d659e2a9bfb922
Jaydonje97 <74739311+Jaydonje97@users.noreply.github.com>
2025-02-24 17:50:03 -0800

    Laid out testing framework, No functions defined yet

---
+++ PetActions.h
@@ -0,0 +1,8 @@
+//
+// Created by jaydo on 2/22/2025.
+//
+
+#ifndef MYPROJECT_PETACTIONS_H
+#define MYPROJECT_PETACTIONS_H
+
+#endif //MYPROJECT_PETACTIONS_H
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jaydo/CLionProjects/PetSimulator
+# Build directory: C:/Users/jaydo/CLionProjects/PetSimulator/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(PetActionsTest "C:/Users/jaydo/CLionProjects/PetSimulator/cmake-build-debug/PetActionsTest.exe")
+set_tests_properties(PetActionsTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jaydo/CLionProjects/PetSimulator/CMakeLists.txt;27;add_test;C:/Users/jaydo/CLionProjects/PetSimulator/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
